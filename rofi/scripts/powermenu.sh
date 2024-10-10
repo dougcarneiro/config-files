509a539dde2e6127572075fc867f1cf2820952da
Douglas Carneiro <douglas.ul@gmail.com>
2024-10-10 11:14:26 -0300

    adjusting rofi themes

--- rofi/scripts/powermenu.sh
+++ rofi/scripts/powermenu.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Definir as opções do menu
-OPTIONS="  Suspend\n  Logout\n  Restart\n  Poweroff"
+OPTIONS=" Suspend\n Logout\n Restart\n Poweroff"
 
 # Mostrar o menu e capturar a escolha do usuário
-CHOICE=$(echo -e "$OPTIONS" | rofi -dmenu -p "Escolha uma opção:" -i -theme powermenu)
+CHOICE=$(echo -e "$OPTIONS" | rofi -dmenu -p "Power Menu" -i -theme powermenu)
 
 # Executar comandos com base na escolha
 case "$CHOICE" in
-    "  Suspend")
+    " Suspend")
         systemctl suspend
         ;;
-    "  Logout")
+    " Logout")
         i3-msg exit
         ;;
-    "  Restart")
+    " Restart")
         systemctl reboot
         ;;
-    "  Poweroff")
+    " Poweroff")
         systemctl poweroff
         ;;
     *)
