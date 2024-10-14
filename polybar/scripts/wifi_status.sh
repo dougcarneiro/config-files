4c59e04ea77fa786879b5b754ea794045cb61d8b
Douglas Carneiro <douglas.ul@gmail.com>
2024-10-13 22:25:55 -0300

    adjusments

--- polybar/scripts/wifi_status.sh
+++ polybar/scripts/wifi_status.sh
@@ -6,7 +6,7 @@ wifi_status=$(nmcli -t -f WIFI g)
 
 ssid=$(nmcli -t -f ACTIVE,SSID dev wifi | grep '^yes' | cut -d':' -f2)
 
-icon_disconnected="NO WIFI"
+icon_disconnected="wifi off"
 icon_connected=""
 
 if [[ "$wifi_status" == "enabled" && -n "$ssid" ]]; then
