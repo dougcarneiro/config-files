b476cf4aac46242114f5a420eabead7f3ed95ee6
Douglas Carneiro <douglas.ul@gmail.com>
2024-10-15 17:36:18 -0300

    polybar now shows bluetooth headphone battery percentage

--- polybar/scripts/bluetooth.sh
+++ polybar/scripts/bluetooth.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# Verifica se o Bluetooth está ligado
 power_status=$(bluetoothctl show | grep "Powered: yes")
-
-# Verifica se existe algum dispositivo conectado
 device_connected=$(bluetoothctl info | grep "Connected: yes")
+audio_output_mac_address=$(pactl list sinks short | grep bluez_output | sed -E 's/.*bluez_output\.([0-9A-Fa-f_]+)\..*/\1/' | sed 's/_/:/g')
 
 if [ "$power_status" ]; then
     if [ "$device_connected" ]; then
-        # Ícone para Bluetooth ligado e dispositivo conectado
-        echo " "
+        device_audio=$(bluetoothctl info "$audio_output_mac_address" | grep "Audio Sink")
+        # Checks if connected device supports audio sink
+        if [ "$device_audio" ]; then
+            # Checks if connected device outputs its battery percentage
+            device_percentage=$(bluetoothctl info "$audio_output_mac_address" | grep "Battery" | awk '{print $4}' | tr -d '()')
+            if [ "$device_percentage" ]; then
+                echo " $device_percentage%"
+            fi
+        else
+            echo " "
+        fi
     else
-        # Ícone para Bluetooth ligado, mas sem dispositivos conectados
         echo ""
     fi
 else
-    # Ícone para Bluetooth desligado
     echo ""
 fi
 
