780b24a8286115d08663a034eedbb576d0c8f630
Douglas Carneiro <douglas.ul@gmail.com>
2024-10-17 23:07:06 -0300

    adjustment

--- i3/scripts/config-touchpad.sh
+++ i3/scripts/config-touchpad.sh
@@ -4,4 +4,4 @@ TOUCHPAD_ID=$(xinput list | grep Touchpad | awk '{print $6}' | sed 's/id=//')
 
 xinput set-prop $TOUCHPAD_ID "libinput Natural Scrolling Enabled" 1
 xinput set-prop $TOUCHPAD_ID "libinput Tapping Enabled" 1
-xinput setprop 11 "libinput Accel Speed" 0.250
+xinput setprop $TOUCHPAD_ID "libinput Accel Speed" 0.250
