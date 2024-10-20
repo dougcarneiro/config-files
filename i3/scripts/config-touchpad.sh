f797bb12deaea42a708ac7cf44da02b27f2fa9a1
Douglas Carneiro <douglas.ul@gmail.com>
2024-10-20 15:03:50 -0300

    fix touchpad config

--- i3/scripts/config-touchpad.sh
+++ i3/scripts/config-touchpad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TOUCHPAD_ID=$(xinput list | grep Touchpad | awk '{print $6}' | sed 's/id=//')
+TOUCHPAD_ID=$(xinput list | grep -i touchpad | awk '{print $6}' | sed 's/id=//')
 
 xinput set-prop $TOUCHPAD_ID "libinput Natural Scrolling Enabled" 1
 xinput set-prop $TOUCHPAD_ID "libinput Tapping Enabled" 1
-xinput setprop $TOUCHPAD_ID "libinput Accel Speed" 0.250
+xinput set-prop $TOUCHPAD_ID "libinput Accel Speed" 0.250
