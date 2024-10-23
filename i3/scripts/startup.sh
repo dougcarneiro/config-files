2464958fcbcee6aeca169071a4198f83d9966a1e
Douglas Carneiro <douglas.ul@gmail.com>
2024-10-23 19:17:10 -0300

    adding lock screen image and adding lock screen key bind to i3 config

--- i3/scripts/startup.sh
+++ i3/scripts/startup.sh
@@ -13,3 +13,10 @@ then
     picom &
 fi
 
+# Creating lock image
+# We use maim to take a picture of the screen, then we give it a pixelated feel with gm
+IMG="/tmp/lock_screen.png" && maim "$IMG" && gm convert $IMG -scale 10% -scale 1000% -fill black -colorize 25% $IMG
+
+# xss-lock grabs a logind suspend inhibit lock and will use i3lock to lock the
+# screen before suspend. Use loginctl lock-session to lock your screen.
+exec --no-startup-id xss-lock --transfer-sleep-lock -- i3lock --nofork -i /tmp/lock_screen.png
