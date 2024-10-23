2dd068d849fb131b1a229a4b38d42277e9b8f25c
Douglas Carneiro <douglas.ul@gmail.com>
2024-10-23 16:29:44 -0300

    moving picom init to startup script

--- i3/scripts/startup.sh
+++ i3/scripts/startup.sh
@@ -5,3 +5,11 @@
 ~/.config/i3/scripts/config-touchpad.sh
 ~/.config/i3/scripts/keyboard.sh
 ~/.config/i3/scripts/set-wallpaper.sh
+
+# Making sure to start picom after xrandr configs are all set and done
+if ! pgrep -x "picom" > /dev/null
+then
+    # Start picom only if it hasn't been initiated yet
+    picom &
+fi
+
