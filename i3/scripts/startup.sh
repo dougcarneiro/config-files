23a947ef5b1101b6b6b5b89d349cc738ed340a27
Douglas Carneiro <douglas.ul@gmail.com>
2024-10-24 17:02:45 -0300

    adjusting xss-lock on startup shell script

--- i3/scripts/startup.sh
+++ i3/scripts/startup.sh
@@ -19,4 +19,9 @@ IMG="/tmp/lock_screen.png" && maim "$IMG" && gm convert $IMG -scale 10% -scale 1
 
 # xss-lock grabs a logind suspend inhibit lock and will use i3lock to lock the
 # screen before suspend. Use loginctl lock-session to lock your screen.
-exec --no-startup-id xss-lock --transfer-sleep-lock -- i3lock --nofork -i /tmp/lock_screen.png
+if ! pgrep -x "xss-lock" > /dev/null
+then
+    # Start xss-lock only if it hasn't been initiated yet
+    xss-lock --transfer-sleep-lock -- i3lock --nofork -i /tmp/lock_screen.png &
+fi
+
